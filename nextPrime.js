9ecb04f6d507a8684b91714a58d136020d600095
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-10 12:55:39 -0700

    initial commit

---
+++ nextPrime.js
@@ -0,0 +1,13 @@
+let nextPrime = function(number) {
+
+}
+
+
+
+
+console.log(nextPrime(2)); // 3
+console.log(nextPrime(3)); // 5
+console.log(nextPrime(7)); // 11
+console.log(nextPrime(8)); // 11
+console.log(nextPrime(20)); // 23
+console.log(nextPrime(97)); // 101
