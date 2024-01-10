969cdddec075b1a7b5dcf472df560d52c90e36c6
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-10 13:37:51 -0700

    initial commit

---
+++ isAntiPrime.js
@@ -0,0 +1,14 @@
+// Your code here
+
+
+
+console.log(isAntiPrime(24))   // true
+console.log(isAntiPrime(36))   // true
+console.log(isAntiPrime(48))   // true
+console.log(isAntiPrime(360))  // true
+console.log(isAntiPrime(1260)) // true
+console.log(isAntiPrime(27))   // false
+console.log(isAntiPrime(5))    // false
+console.log(isAntiPrime(100))  // false
+console.log(isAntiPrime(136))  // false
+console.log(isAntiPrime(1024)) // false
