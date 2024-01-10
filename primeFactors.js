014cc5c9ba29f3377a718d315dc76e932c5a71c5
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-10 13:14:06 -0700

    initial commit

---
+++ primeFactors.js
@@ -0,0 +1,11 @@
+// Your code here
+
+
+
+console.log(primeFactors(12));  // [2, 3]
+console.log(primeFactors(7));   // [7]
+console.log(primeFactors(16));  // [2]
+console.log(primeFactors(30));  // [2, 3, 5]
+console.log(primeFactors(35));  // [5, 7]
+console.log(primeFactors(49));  // [7]
+console.log(primeFactors(128)); // [2]
