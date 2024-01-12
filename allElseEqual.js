2696aa31b30e3c81122e72ba3ef2e5ee4e6a58ef
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-12 12:44:52 -0700

    added sumArray

--- allElseEqual.js
+++ allElseEqual.js
@@ -2,6 +2,14 @@ function allElseEqual(arr) {
   // your code here
 }
 
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
+}
+
 console.log(allElseEqual([2, 4, 3, 10, 1])); // 10
 console.log(allElseEqual([6, 3, 5, -9, 1])); // 3
 console.log(allElseEqual([1, 2, 3, 4]));     // null
