60d7f79b458923d0bf34b3d61a722c569220cec4
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-11 19:22:13 -0700

    added countVowels

--- mostVowels.js
+++ mostVowels.js
@@ -2,4 +2,17 @@ function mostVowels(sentence) {
   // your code here
 }
 
+function countVowels(word) {
+  let count = 0;
+  let vowels = "aeiou";
+  for (i = 0; i < word.length; i++) {
+    char = word[i];
+    if (vowels.includes(char)) {
+      count++
+    }
+  }
+  return count;
+};
+
+
 console.log(mostVowels("what a wonderful life")); // "wonderful"
